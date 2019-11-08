fdf6aae13bb5e4a81fa6c1c52e457914c3bc58df
Chandraleka <chandralekasrinivasan@gmail.com>
2019-11-08 21:35:09 +0530

    Updates

---
+++ precache-manifest.1dc2023e0d0684f5086054690f389eba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac304460a0650cecba4342c781dc496",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "d1faf7ec43a311e0e237",
+    "url": "/portfolio-site/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "ac9ca36dd8ed66c4f6b5",
+    "url": "/portfolio-site/static/css/main.4bc88f44.chunk.css"
+  },
+  {
+    "revision": "d1faf7ec43a311e0e237",
+    "url": "/portfolio-site/static/js/2.737bd90c.chunk.js"
+  },
+  {
+    "revision": "ac9ca36dd8ed66c4f6b5",
+    "url": "/portfolio-site/static/js/main.a4987255.chunk.js"
+  },
+  {
+    "revision": "0e91e67b0343f82b7ff1",
+    "url": "/portfolio-site/static/js/runtime-main.9fe206bc.js"
+  },
+  {
+    "revision": "8bdac0dc847401cd249df4150a77de34",
+    "url": "/portfolio-site/static/media/girlsWhoCode.8bdac0dc.png"
+  },
+  {
+    "revision": "04f22177f28b04049a8752a10b8e8b65",
+    "url": "/portfolio-site/static/media/ideas.04f22177.png"
+  },
+  {
+    "revision": "790465a76bfa3b218d53c146f5fdab6c",
+    "url": "/portfolio-site/static/media/learning.790465a7.png"
+  }
+]);
